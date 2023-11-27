bd43e975eea645c8fe938fbc8e56057a7d824ee7
Nicolas Voigt <FZCeuropeu@gmail.com>
2023-11-27 11:01:33 -0300

    PedraPapelTesoura

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    let match = player1 + player2
+    if ((match === "PedraTesoura")||(match === "TesouraPapel")||(match === "PapelPedra")){
+        return "Jogador 1 venceu!"
+    } else if ((match === "PedraPedra")||(match === "TesouraTesoura")||(match === "PapelPapel")){
+        return "Empate!"
+    } else if ((match === "PedraPapel")||(match === "TesouraPedra")||(match === "PapelTesoura")){
+        return "Jogador 2 venceu!"
+    }
+}
